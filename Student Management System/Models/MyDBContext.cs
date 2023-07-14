6dfeeecd25b3fde0fa8c7995d37222fcb2c90270
rohanshresthaa <r.shrestharohan@gmail.com>
2023-07-14 08:43:24 +0545

    chores: dependencies added

---
+++ Student Management System/Models/MyDBContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Student_Management_System.Models
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> context):base(context) 
+        {
+            
+        }
+        public DbSet<SignUp> signUps { get; set; }
+    }
+}
---
+++ Student Management System/Models/SignUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Student_Management_System.Models
+{
+    public class SignUp
+    {
+        public int? Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNo { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
